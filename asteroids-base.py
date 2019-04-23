52a90c068f837b12f4955b4e4d30876a9e61b8aa
Thomas Bekhor <tbekhor@Thomass-MacBook-Air.local>
2019-04-23 17:17:02 -0300

    parte 1

--- asteroids-base.py
+++ asteroids-base.py
@@ -20,6 +20,19 @@ GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
 
+class Player(pygame.sprite.Sprite):
+    
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        player_img=pygame.image.load(path.join(img_dir, "playerShip1_orange.png")).convert()
+        self.image=player_img
+        self.image=pygame.transform.scale(player_img, (50,38))
+        self.image.set_colorkey(BLACK)
+        self.rect=self.image.get_rect()
+        self.rect.centerx=WIDTH/2
+        self.rect.bottom=HEIGHT- 10
+        self.speedx=0
+        
 # Inicialização do Pygame.
 pygame.init()
 pygame.mixer.init()
@@ -37,6 +50,11 @@ clock = pygame.time.Clock()
 background = pygame.image.load(path.join(img_dir, 'starfield.png')).convert()
 background_rect = background.get_rect()
 
+player=Player()
+
+all_sprites=pygame.sprite.Group()
+all_sprites.add(player)
+
 # Comando para evitar travamentos.
 try:
     
@@ -53,11 +71,12 @@ try:
             # Verifica se foi fechado
             if event.type == pygame.QUIT:
                 running = False
+            if event.type ==pygame.KEYDOWN:
     
         # A cada loop, redesenha o fundo e os sprites
         screen.fill(BLACK)
         screen.blit(background, background_rect)
-        
+        all_sprites.draw(screen)
         # Depois de desenhar tudo, inverte o display.
         pygame.display.flip()
         
